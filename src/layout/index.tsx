9155304181cbcaa88d30782d90445396720276d6
hefx <1074535830@qq.com>
2022-11-21 14:55:23 +0800

    添加sidebar组件

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,10 +1,12 @@
 import React, { FC } from "react";
 import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
 
 const Layout: FC = () => {
   return (
     <div>
       <Navbar />
+      <Sidebar />
     </div>
   );
 };
